2a5309f3ee22e1faed43f74d4509df85d19ea842
Error916 <ettorenigma@gmail.com>
2023-03-21 14:15:09 +0100

    added the dump_f64 function

--- src/easm2nasm.c
+++ src/easm2nasm.c
@@ -33,8 +33,8 @@ int main(int argc, char **argv) {
 		Inst inst = easm.program[i];
 
 		if (i == easm.entry) {
-            		printf("_start:\n");
-        	}
+	    		printf("_start:\n");
+		}
 
 		printf("inst_%zu:\n", i);
 		switch (inst.type) {
@@ -43,15 +43,16 @@ int main(int argc, char **argv) {
 			case INST_PUSH: {
 				printf("\t;; push %lu\n", inst.operand.as_u64);
 				printf("\tmov rsi, [stack_top]\n");
-				printf("\tmov QWORD [rsi], %lu\n", inst.operand.as_u64);
+				printf("\tmov rax, 0x%lx\n", inst.operand.as_u64);
+				printf("\tmov QWORD [rsi], rax\n");
 				printf("\tadd QWORD [stack_top], EVM_WORD_SIZE\n");
 			} break;
 
 			case INST_DROP: {
 				printf("\t;; drop\n");
 				printf("\tmov rsi, [stack_top]\n");
-            			printf("\tsub rsi, EVM_WORD_SIZE\n");
-            			printf("\tmov [stack_top], rsi\n");
+	    			printf("\tsub rsi, EVM_WORD_SIZE\n");
+	    			printf("\tmov [stack_top], rsi\n");
 			} break;
 
 			case INST_DUP: {
@@ -104,23 +105,23 @@ int main(int argc, char **argv) {
 			} break;
 
 			case INST_MULTI: {
-            			printf("\t;; FIXME: multi\n");
-        		} break;
+	    			printf("\t;; FIXME: multi\n");
+			} break;
 
 			case INST_MULTU: UNIMPLEMENTED("INST_MULTU");
 
 			case INST_DIVI: {
-            			printf("\t;; divi\n");
-            			printf("\tmov rsi, [stack_top]\n");
-            			printf("\tsub rsi, EVM_WORD_SIZE\n");
-            			printf("\tmov rbx, [rsi]\n");
-            			printf("\tsub rsi, EVM_WORD_SIZE\n");
-            			printf("\tmov rax, [rsi]\n");
-            			printf("\txor rdx, rdx\n");
-            			printf("\tidiv rbx\n");
-            			printf("\tmov [rsi], rax\n");
-            			printf("\tadd rsi, EVM_WORD_SIZE\n");
-            			printf("\tmov [stack_top], rsi\n");
+	    			printf("\t;; divi\n");
+	    			printf("\tmov rsi, [stack_top]\n");
+	    			printf("\tsub rsi, EVM_WORD_SIZE\n");
+	    			printf("\tmov rbx, [rsi]\n");
+	    			printf("\tsub rsi, EVM_WORD_SIZE\n");
+	    			printf("\tmov rax, [rsi]\n");
+	    			printf("\txor rdx, rdx\n");
+	    			printf("\tidiv rbx\n");
+	    			printf("\tmov [rsi], rax\n");
+	    			printf("\tadd rsi, EVM_WORD_SIZE\n");
+	    			printf("\tmov [stack_top], rsi\n");
 			} break;
 
 			case INST_DIVU: {
@@ -135,7 +136,7 @@ int main(int argc, char **argv) {
 				printf("\tmov [rsi], rax\n");
 				printf("\tadd rsi, EVM_WORD_SIZE\n");
 				printf("\tmov [stack_top], rsi\n");
-        		} break;
+			} break;
 
 			case INST_MODI: {
 				printf("\t;; modi\n");
@@ -163,12 +164,24 @@ int main(int argc, char **argv) {
 				printf("\tmov [rsi], rdx\n");
 				printf("\tadd rsi, EVM_WORD_SIZE\n");
 				printf("\tmov [stack_top], rsi\n");
-        		} break;
+			} break;
 
-			case INST_PLUSF: UNIMPLEMENTED("INST_PLUSF");
-			case INST_MINUSF: UNIMPLEMENTED("INST_MINUSF");
-			case INST_MULTF: UNIMPLEMENTED("INST_MULTF");
-			case INST_DIVF: UNIMPLEMENTED("INST_DIVF");
+			case INST_PLUSF: {
+	    			printf("\t;; FIXME: plusf\n");
+			} break;
+
+			case INST_MINUSF: {
+	    			printf("\t;; FIXME: minusf\n");
+			} break;
+
+			case INST_MULTF: {
+	    			printf("\t;; FIXME: multf\n");
+			} break;
+
+			case INST_DIVF: {
+	    			printf("\t;; FIXME: divf\n");
+			} break;
+
 			case INST_JMP: {
 				printf("\t;; jmp\n");
 				printf("\tmov rdi, inst_map\n");
@@ -260,23 +273,23 @@ int main(int argc, char **argv) {
 			} break;
 
 			case INST_GEI: {
-            			printf("\t;; FIXME: gei\n");
-        		} break;
+	    			printf("\t;; FIXME: gei\n");
+			} break;
 
 			case INST_GTI: {
-            			printf("\t;; FIXME: gti\n");
-        		} break;
+	    			printf("\t;; FIXME: gti\n");
+			} break;
 
 			case INST_LEI: {
-            			printf("\t;; FIXME: lei\n");
-        		} break;
+	    			printf("\t;; FIXME: lei\n");
+			} break;
 
 			case INST_LTI: UNIMPLEMENTED("INST_LTI");
 			case INST_NEI: UNIMPLEMENTED("INST_NEI");
 
-        		case INST_EQU: {
-            			printf("\t;; FIXME: equ\n");
-        		} break;
+			case INST_EQU: {
+	    			printf("\t;; FIXME: equ\n");
+			} break;
 
 			case INST_GEU: UNIMPLEMENTED("INST_GEU");
 			case INST_GTU: UNIMPLEMENTED("INST_GTU");
@@ -284,14 +297,35 @@ int main(int argc, char **argv) {
 			case INST_LTU: UNIMPLEMENTED("INST_LTU");
 			case INST_NEU: UNIMPLEMENTED("INST_NEU");
 			case INST_EQF: UNIMPLEMENTED("INST_EQF");
-			case INST_GEF: UNIMPLEMENTED("INST_GEF");
-			case INST_GTF: UNIMPLEMENTED("INST_GTF");
-			case INST_LEF: UNIMPLEMENTED("INST_LEF");
-			case INST_LTF: UNIMPLEMENTED("INST_LTF");
+
+			case INST_GEF: {
+			    	printf("\t;; FIXME: gef\n");
+			} break;
+
+			case INST_GTF: {
+			    	printf("\t;; FIXME: gtf\n");
+			} break;
+
+			case INST_LEF: {
+			    	printf("\t;; FIXME: lef\n");
+			} break;
+
+			case INST_LTF: {
+			    	printf("\t;; FIXME: ltf\n");
+			} break;
+
 			case INST_NEF: UNIMPLEMENTED("INST_NEF");
-			case INST_ANDB: UNIMPLEMENTED("INST_ANDB");
+
+			case INST_ANDB: {
+	    			printf("\t;; FIXME: andb\n");
+			} break;
+
 			case INST_ORB: UNIMPLEMENTED("INST_ORB");
-			case INST_XOR: UNIMPLEMENTED("INST_XOR");
+
+			case INST_XOR: {
+	    			printf("\t;; FIXME: xor\n");
+			} break;
+
 			case INST_SHR: UNIMPLEMENTED("INST_SHR");
 			case INST_SHL: UNIMPLEMENTED("INST_SHL");
 			case INST_NOTB: UNIMPLEMENTED("INST_NOTB");
@@ -326,9 +360,17 @@ int main(int argc, char **argv) {
 			case INST_WRITE16: UNIMPLEMENTED("INST_WRITE16");
 			case INST_WRITE32: UNIMPLEMENTED("INST_WRITE32");
 			case INST_WRITE64: UNIMPLEMENTED("INST_WRITE64");
-			case INST_I2F: UNIMPLEMENTED("INST_I2F");
+
+			case INST_I2F: {
+	    			printf("\t;; FIXME: i2f\n");
+			} break;
+
 			case INST_U2F: UNIMPLEMENTED("INST_U2F");
-			case INST_F2I: UNIMPLEMENTED("INST_F2I");
+
+			case INST_F2I: {
+			    	printf("\t;; FIXME: f2i\n");
+			} break;
+
 			case INST_F2U: UNIMPLEMENTED("INST_F2U");
 
 			case INST_HALT: {
@@ -348,18 +390,18 @@ int main(int argc, char **argv) {
 	printf("stack_top: dq stack\n");
 	printf("inst_map: dq");
     	for (size_t i = 0; i < easm.program_size; ++i) {
-        	printf(" inst_%zu,", i);
+		printf(" inst_%zu,", i);
     	}
 	printf("\n");
     	printf("memory:\n");
 #define ROW_SIZE 10
 #define ROW_COUNT(size) ((size + ROW_SIZE - 1) / ROW_SIZE)
     	for (size_t row = 0; row < ROW_COUNT(easm.memory_size); ++row) {
-        	printf("\tdb");
-        	for (size_t col = 0; col < ROW_SIZE && row * ROW_SIZE + col < easm.memory_size; ++col) {
-        		printf(" %u,", easm.memory[row * ROW_SIZE + col]);
-        	}
-        	printf("\n");
+		printf("\tdb");
+		for (size_t col = 0; col < ROW_SIZE && row * ROW_SIZE + col < easm.memory_size; ++col) {
+			printf(" %u,", easm.memory[row * ROW_SIZE + col]);
+		}
+		printf("\n");
     	}
     	// TODO: warning: uninitialized space declared in non-BSS section `.data': zeroing
     	//   Is it possible to get rid of it? Not really suppress it, but just let nasm know
